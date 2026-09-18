a0f8ab139cdd67945b21fce6473d673cde7f265a
JuanM07-sena <juanmsena07@gmail.com>
2026-09-18 16:43:29 -0500

    refactor: mover OperacionUltimaMillaApplication al paquete raíz

--- src/main/java/com/suministrosnorte/OperacionUltimaMillaApplication.java
+++ src/main/java/com/suministrosnorte/OperacionUltimaMillaApplication.java
@@ -1,10 +1,10 @@
-package com.suministrosnorte.operacion_ultima_milla;
+package com.suministrosnorte;
 
 // Punto de entrada: inicia Spring Boot y registra entidades, controladores y repositorios.
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.suministrosnorte")
